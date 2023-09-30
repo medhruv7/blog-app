7cc74595d8e9583d58da70da1744cb0e34f180d6
= <=>
2023-09-30 13:35:17 +0530

    fixed import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Navbar } from "./components/Navbar";
-import { Providers } from "./Providers/providers";
+import { Providers } from "./Providers/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
